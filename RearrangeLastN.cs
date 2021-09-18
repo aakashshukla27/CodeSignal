b813862570ca5d5eb236e29204773b4b270ec07f
Aakash Shukla <shukla.aa@northeastern.edu>
2021-09-18 16:20:49 -0400

    Added LinkedList

---
+++ RearrangeLastN.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal
+{
+    class RearrangeLastN
+    {
+        static ListNode<int> rearrangeLastN(ListNode<int> l, int n)
+        {
+            ListNode<int> c = l;
+            ListNode<int> prev = l;
+            int length = GetLength(l);
+            for (int i = 0; i < length - n; i++)
+            {
+                prev = l;
+                l = l.next;
+            }
+
+            prev.next = null;
+            ListNode<int> temp = l;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = c;
+
+
+            //prev.next = null;
+
+
+            return null;
+        }
+
+        static int GetLength(ListNode<int> a)
+        {
+            int count = 0;
+            while (a != null)
+            {
+                a = a.next;
+                count++;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode<int> a = new ListNode<int>(1);
+            a.next = new ListNode<int>(2);
+            a.next.next = new ListNode<int>(3);
+            a.next.next.next = new ListNode<int>(4);
+            a.next.next.next.next = new ListNode<int>(5);
+            rearrangeLastN(a, 3);
+        }
+    }
+}
---
+++ ReverseNodesInKGroups.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal
+{
+    class ReverseNodesInKGroups
+    {
+        static ListNode<int> reverseNodesInKGroups(ListNode<int> l, int k)
+        {
+            int count = 0;
+            if (l == null) return null;
+            ListNode<int> current = l;
+            ListNode<int> next = null, prev = null;
+            if (GetLength(l) < k)
+            {
+                return l;
+            }
+            while (count < k && current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+                count++;
+            }
+            if(next != null)
+            {
+                l.next = reverseNodesInKGroups(next, k);
+            }
+            return prev;
+        }
+        static int GetLength(ListNode<int> t)
+        {
+            int count = 0;
+            while (t != null)
+            {
+                count++;
+                t = t.next;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
