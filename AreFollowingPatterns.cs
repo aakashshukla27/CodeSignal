7ff9312bb359c6716e015d9bced4ed891b1a3159
Aakash Shukla <shukla.aa@northeastern.edu>
2021-09-20 17:16:52 -0400

    added hash tables

---
+++ AreFollowingPatterns.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal
+{
+    class AreFollowingPatterns
+    {
+        static bool areFollowingPatterns(string[] strings, string[] patterns)
+        {
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            Dictionary<string, string> dict2 = new Dictionary<string, string>();
+
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                if (!dict1.ContainsKey(patterns[i]) )
+                {
+                    dict1.Add(patterns[i], strings[i]);
+                }
+                if (!dict2.ContainsKey(strings[i]))
+                {
+                    dict2.Add(strings[i], patterns[i]);
+                }
+                if (dict1.ContainsKey(patterns[i]))
+                {
+                    string temp = dict1[patterns[i]];
+                    if (temp != strings[i]) return false;
+                }
+                if (dict2.ContainsKey(strings[i]))
+                {
+                    string temp = dict2[strings[i]];
+                    if (temp != patterns[i]) return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] strings = { "kwtfpzm",
+ "kwtfpzm",
+ "kwtfpzm",
+ "kwtfpzm",
+ "kwtfpzm",
+ "wfktjrdhu",
+ "anx",
+ "kwtfpzm" };
+            string[] patterns = { "z",
+ "z",
+ "z",
+ "hhwdphhnc",
+ "zejhegjlha",
+ "xgxpvhprdd",
+ "e",
+ "u" };
+            areFollowingPatterns(strings, patterns);
+        }
+    }
+}
---
+++ GroupingDishes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal
+{
+    class GroupingDishes
+    {
+        static string[][] groupingDishes(string[][] dishes)
+        {
+            SortedDictionary<string, int> dishDict = new SortedDictionary<string, int>();
+            HashSet<string> temp = new HashSet<string>();
+            foreach(var item in dishes)
+            {
+                for(int i=1; i<item.Length; i++)
+                {
+
+                    if (!dishDict.ContainsKey(item[i]))
+                    {
+                        dishDict.Add(item[i], 1);
+                    }
+                    else
+                    {
+                        dishDict[item[i]]++;
+                    }
+                }
+            }
+            var newDish = dishDict.Where(s => s.Value > 1).ToDictionary(s=>s.Key, s=>s.Value);
+            List<string> keyList = new List<string>(newDish.Keys);
+
+            List<List<string>> returnList = new List<List<string>>();
+            for(int i=0; i<keyList.Count; i++)
+            {
+                List<string> tempStore = new List<string>();
+                foreach(var item in dishes)
+                {
+                    if (item.Contains(keyList[i]))
+                    {
+                        tempStore.Add(item[0]);
+                    }
+                }
+                tempStore.Sort();
+                returnList.Add(tempStore);
+            }
+            string[][] returnList2 = new string[keyList.Count][];
+
+            for(int i =0; i<keyList.Count; i++)
+            {
+                returnList2[i] = new string[returnList[i].Count + 1];
+                returnList2[i][0] = keyList[i];
+                for(int j=0; j<returnList[i].Count; j++)
+                {
+                    returnList2[i][j + 1] = returnList[i][j];
+                }
+            }
+            return returnList2;
+        }
+
+        static void Main(string[] args)
+        {
+            string[][] dishes = new string[4][];
+            dishes[0] = new string[] { "Salad", "Tomato", "Cucumber", "Salad", "Sauce" };
+            dishes[1] = new string[] { "Pizza", "Tomato", "Sausage", "Sauce", "Dough" };
+            dishes[2] = new string[] { "Quesadilla", "Chicken", "Cheese", "Sauce" };
+            dishes[3] = new string[] { "Sandwich", "Salad", "Bread", "Tomato", "Cheese" };
+
+            groupingDishes(dishes);
+        }
+    }
+}
